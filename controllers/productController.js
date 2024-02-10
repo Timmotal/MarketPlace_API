b3e2d6096c66e497d5af19b8f0a102af032918f8
unknown <timmortal44@yahoo.com>
2024-02-10 10:19:36 +0000

    finalize the fetch all products routes

--- controllers/productController.js
+++ controllers/productController.js
@@ -9,3 +9,12 @@ const createProducts = async (req, res) => {
     // he likes to send empty values first-> so he can see that his validation works
     // res.send('createProducts ... 1845 hours, Feb 9th, 2024');
 };
+
+const getAllProducts = async (req, res) => {
+    const products = await Product.find({});
+
+    res.status(StatusCodes.OK).json({ products, count: products.length });
+    // res.send('getAllProducts ... 1845 hours, Feb 9th, 2024');
+};
+
+
