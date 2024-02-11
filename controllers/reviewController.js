b594bf8041e8542fa6d349e9c49ae3ccfe9c2a58
unknown <timmortal44@yahoo.com>
2024-02-11 22:35:05 +0000

    setup create review

---
+++ controllers/reviewController.js
@@ -0,0 +1,33 @@
+const Product = require('../models/Product');
+const Review = require('../models/Review');
+
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+const { checkPermissions } = require('../utils');
+
+const createReview = async (req, res) => {
+    const { product: productId } = req.body; // productId is the alias here
+    // check if there is a product ID
+    const isValidProduct = await Product.findOne({ _id: productId});
+
+    if (!isValidProduct){
+        throw new CustomError.NotFoundError(`Nope, no Product with id : ${productId}`);
+    }
+    // check if user already submitted a review for this particular product
+    const alreadySubmitted = await Review.findOne({
+        product: productId,
+        user: req.user.userId,
+    });
+
+    if (alreadySubmitted){
+        throw new CustomError.BadRequestError('You formerly reviewed this product ');
+    }
+
+    req.body.user = req.user.userId; // we have the middleware, that is why we can do this
+    const review = await Review.create(req.body);
+    
+    res.status(StatusCodes.CREATED).json({ review });
+    // res.send('createReview. Feb 10th, 2024, what are you running from, rejection?. course is taking way too long');
+};
+;
+
